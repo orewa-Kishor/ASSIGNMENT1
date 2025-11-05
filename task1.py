4266a1ea5a2ad33921d057c678854ca51c7f4dd3
kishor ghanchi <kishorganchi.ic25@scet.ac.in>
2025-11-05 10:49:07 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+# 1. Swap two numbers
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+addition = a + b
+print(addition)
+subtraction = a - b
+print(subtraction)
+multiplication = a * b
+print(multiplication)
+division = a / b
+print(division)
---
+++ task2.py
@@ -0,0 +1,4 @@
+first_name = input("Enter the first name: ")
+last_name = input("Enter the last name: ")
+print(f"The first name is: {first_name} ")
+print(f"The last name is: {last_name} ")
