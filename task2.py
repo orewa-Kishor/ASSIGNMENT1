3891e4df18df70f131fb579b321fde36a69771e3
kishor ghanchi <kishorganchi.ic25@scet.ac.in>
2025-11-15 10:30:06 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,3 @@
 first_name = input("Enter the first name: ")
 last_name = input("Enter the last name: ")
-print(f"The first name is: {first_name} ")
-print(f"The last name is: {last_name} ")
+print(f"Hello , {first_name}{last_name}! Welcome to the Python Program .)
